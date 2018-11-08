d1560018352642160ff3874d132f046c890eafdb
jersearls <searls@me.com>
2018-11-07 23:35:17 -0500

    Configure lambda retry handler to 0

--- cpap_filler/lambda_function.py
+++ cpap_filler/lambda_function.py
@@ -1,10 +1,15 @@
 import logging
 from pumper import Pumper
 from dotenv import load_dotenv
+import boto3
+import botocore.config
+
 
 def lambda_handler(event, context):
     load_dotenv()
     logging.basicConfig(format='%(asctime)s [%(levelname)s] %(name)s: %(message)s')
     logger = logging.getLogger()
     logger.setLevel(logging.DEBUG)
+    cfg = botocore.config.Config(retries={'max_attempts': 3})
+    client = boto3.client('lambda', config=cfg)
     Pumper().run()
